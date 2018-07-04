bacc9bf954546cdc1a71d3bbb6fc857e642f76b0
Joel George V <joel.gv3@gmail.com>
2018-07-04 08:02:41 +0530

    Update solution

--- Arrays/OddOccurrencesInArray.js
+++ Arrays/OddOccurrencesInArray.js
@@ -2,13 +2,12 @@ solution = (A) => {
   let solution
   console.log('Input: ', A)
   solution = A.reduce((acc, value) => {
-    const index = acc.indexOf(value)
-    index === -1 ? acc.push(value) : acc.splice(index, 1)
+    acc = acc ^ value
     return acc
-  }, [])
-  console.log('Solution: ', solution[0])
+  }, 0)
+  console.log('Solution: ', solution)
 }
 
 solution([9, 3, 9, 3, 9, 3, 9])
 
-// 66%, 100%, 25%
+// 100%
