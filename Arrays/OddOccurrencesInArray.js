ea9ea0a7ad3e733dcd9712feabfd686410abbcb1
Joel George V <joel.gv3@gmail.com>
2018-07-07 21:08:29 +0530

    Update solution

--- Arrays/OddOccurrencesInArray.js
+++ Arrays/OddOccurrencesInArray.js
@@ -1,12 +1,30 @@
 solution = (A) => {
+  let values = {}
   console.log('Input: ', A)
-  const solution = A.reduce((acc, value) => {
-    acc = acc ^ value
+  for (i = 0; i < A.length; i++) {
+    if (values[A[i]] === undefined) {
+      values[A[i]] = false
+    } else if (!values[A[i]]) {
+      values[A[i]] = true
+    } else {
+      values[A[i]] = false
+    }
+  }
+  const solution = Object.keys(values).reduce((acc, value) => {
+    if (!values[value]) {
+      acc = parseInt(value)
+    }
     return acc
   }, 0)
   console.log('Solution: ', solution)
+
+  // Optimum solution
+  /* const solution = A.reduce((acc, value) => {
+    acc = acc ^ value
+    return acc
+  }, 0) */
 }
 
 solution([9, 3, 9, 3, 9, 3, 9])
 
-// 100%
+// 77%, 100%, 50%
